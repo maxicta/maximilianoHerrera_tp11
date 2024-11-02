ce4862bd411001ac1f65a5ca836f9071e670ba6c
Maximiliano Herrera <herreramaximiliano@hotmail.com>
2024-11-01 23:56:51 -0300

    se agrego el archivo tarifa.js

---
+++ maximilianoHerrera_tp11/tarifa.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+let pagoMes = 12000
+let consumoKWH = 450
+
+consumoKWH > 300 ? pagoMes*1.2 : pagoMes;
+
+console.log(`Debido a que su hogar tuvo un consumo de ${consumoKWH} kwh, en base al ajuste tarifario (hogares con consumo mayor a 300kwh por mes tendran un aumento del 20%),cumplimos con informarle que se ha ajustado el total a pagar, que sera de ${pagoMes*1.2}`);
