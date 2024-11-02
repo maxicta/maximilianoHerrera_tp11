c5a35a0691a58e288afcfa6d5e98c32ca2aba962
Maximiliano Herrera <herreramaximiliano@hotmail.com>
2024-11-02 00:05:09 -0300

    se agrego archivo traductor

---
+++ maximilianoHerrera_tp11/traductor.js
@@ -0,0 +1,29 @@
+let palabra = "perros"
+
+switch (palabra) {
+    case "perro":
+        console.log("dog");
+        
+        break;
+    case "gato":
+        console.log("cat");
+        
+        break;
+    case "puerta":
+        console.log("door");
+        
+        break;
+    case "ventana":
+        console.log("window");
+        
+        break;
+    case "mesa":
+        console.log("table");
+        
+        break;
+
+    default:
+        console.log("La palabra ingresada no es correcta");
+        
+        break;
+}
