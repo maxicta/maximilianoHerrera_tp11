fd4eb0419367a1cd9102d1d1e63d759f176cadc2
Maximiliano Herrera <herreramaximiliano@hotmail.com>
2024-11-03 17:20:33 -0300

    se agrego archivo aterrizar avion

---
+++ maximilianoHerrera_tp11/aterrizarAvion.js
@@ -0,0 +1,13 @@
+let velocidad = 268
+let altura = 150
+
+if (velocidad < 268 && altura < 150) {
+    console.log("El avion no esta listo para comenzar el aterrizaje");
+    
+}else if (velocidad > 278 && altura > 300) {
+    console.log("El avion no esta listo para comenzar el aterrizaje");
+
+} else {
+    console.log("El avion esta listo para comenzar el aterrizaje");
+    
+}
